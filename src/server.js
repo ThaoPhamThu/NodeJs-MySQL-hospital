29cb7383512b9d6bc7b591be8ac7b94539f26a39
Pham Thu Thao <phamthuthao13a22014.2015@gmail.com>
2023-05-22 23:06:47 +0700

    err

--- src/server.js
+++ src/server.js
@@ -3,9 +3,11 @@ import bodyParser from "body-parser"; //Lấy tham số mà phía clients sử d
 import viewEngine from "./config/viewEngine";
 import initWebRoutes from "./route/web";
 import connectDB from "./config/connectDB";
+import cors from 'cors';
 require('dotenv').config(); //giúp chạy process.env
 
 let app = express();
+app.use(cors({origin: true}));
 
 //config app
 
